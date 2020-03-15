faef0a3c4e15859b2c1d2b835d080b7079c55c17
sw1014 <577060970@qq.com>
2020-03-15 19:40:22 +0800

    完成用户界面的基本功能

---
+++ WebContent/js/information.js
@@ -0,0 +1,23 @@
+
+
+ function order()
+ {
+     var name=document.getElementById('name').value;
+     var idcard=document.getElementById('idcard').value;
+     var tel=document.getElementById('tel').value;
+     var number=document.getElementById('number').value;
+     $.ajax({
+		type:'POST', 
+		async:false,
+		data:{"name":name,"idcard":idcard,"tel":tel,"number":number},
+        url:'UserServlet', // 填写servlet地址
+		dataType:'json', //预期的服务器响应的数据类型 
+		success: function(result){ // 当请求成功时运行的函数
+			alert(result);
+		},
+		error:function(result){ //失败的函数
+			alert("请求数据失败");
+		},
+	});
+ }
+
